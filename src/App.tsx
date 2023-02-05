8cc24a0826624a175b5621fa565153250ddee6b4
Julian White <39742688+julianwhite007@users.noreply.github.com>
2023-02-05 11:45:52 -0500

    Initial Commit, working iFrame

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <iframe src="https://app.ideta.io/share?bd=-NAB0Hovy-T6piz-k8PP" title="Remote Buddy" height="800px" width="450px" frameBorder="0"></iframe>
+      </header>
+    </div>
+  );
+}
+
+export default App;
